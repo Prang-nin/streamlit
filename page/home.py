fa66aec0195f1156081e6f1c6a5fe030cba44a01
Prang-nin <53726262+Prang-nin@users.noreply.github.com>
2021-12-29 02:50:10 +0000

    Update home.py

--- page/home.py
+++ page/home.py
@@ -1,11 +1,6 @@
 import streamlit as st
-from PIL import Image
 
 def app():
-    logo = Image.open('Avatar.png')
-    st.image(logo, width = 200)
-    st.markdown('''🏄‍♂️
-    "I thrive on creative challenges and enjoy learning new skills. Life is about living with experiences."
-        **CC.Chayanin**  ''')
+    st.markdown('''🙏 Welcome to multiple-apps project 👉 **Go to Navigation Bar ** 🏄‍♂️ ''')
 
 
