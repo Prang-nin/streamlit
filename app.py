03097573f507a316c4cffd308f1b53a082a2dd8b
Prang-nin <53726262+Prang-nin@users.noreply.github.com>
2022-04-11 02:25:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # custom modules
 from multipage import multipage
-from page import home, dataProfiling, stockApp
+from page import home, dataProfiling, stockApp, ,classification
 
 # create an instance for multipage class
 app = multipage()
@@ -16,6 +16,6 @@ st.title("Multi-APPs Project")
 app.addPage("Home page", home.app)
 app.addPage("Data Profilling", dataProfiling.app)
 app.addPage("Observe stock market", stockApp.app)
-
+app.addPage("Classification Experiments", classification.app)
 # main
 app.run()
